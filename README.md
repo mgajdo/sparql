6489adf438c163f55ee82261d5412b8baed9a2eb
mgajdo <martingajdos@mgajdo-mbp2022.fritz.box>
2024-04-26 08:32:23 +0200

    Refactor code

---
+++ README.md
@@ -0,0 +1,9 @@
+# SPARQL queries to Fedlex data
+
+## Instructions
+1. Insert your SPARQL query inside `sparql_query.txt`.
+2. (optional) Add you custom endpoint and assign a variable in `sparql_endpoints.txt`. Adapt the variable to your endpoint in `main.py` line 11. The default endpoint is `F=https://fedlex.data.admin.ch/sparqlendpoint`.
+2. Run `python main.py` inside the shell at the root directory of the project to run the program.
+
+`query.py` contains the logic
+`display_result.py` renders the code as HTML
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-
 import pandas as pd
-from sparql import query, display_result
+import asyncio
+
+from display_result import display_result
+from query import query
 
 async def main():
-    df = await query("""
+    with open("sparql_query.txt", "r") as file:
+        query_string = file.read()
+    
+    # "F" corresponds to F=https://fedlex.data.admin.ch/sparqlendpoint in the file sparql_endpoints.txt
+    df_result = await query(query_string, "F")
 
-    SELECT DISTINCT ?Prädikat ?Objekt WHERE {
-        
-        <https://fedlex.data.admin.ch/eli/cc/1999/404> ?Prädikat ?Objekt .
-    } 
+    display_result(df_result)
 
-    """, "fedlex_sparqlendpoint")
-
-display_result(df)
-
-# Call the async function
-await main()
+# Ensure that main() is called in an asynchronous context when run as Python module, when the module is run directly, not when it's imported as a module into another script.
+if __name__ == "__main__":
+    asyncio.run(main())
